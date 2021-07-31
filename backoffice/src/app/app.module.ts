1a4f7482bb81a021025b624738a589ef0bc52af2
Raül <raulcs1995@gmail.com>
2021-07-31 11:28:00 +0200

    Implementing profile and geolocation card structures

--- backoffice/src/app/app.module.ts
+++ backoffice/src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './profile/profile.component';
+import { GeolocationComponent } from './geolocation/geolocation.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent,
+    GeolocationComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
